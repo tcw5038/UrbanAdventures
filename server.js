d9bacdf3cbac536be614595067657e71ef5064ae
Tyler Warrick <tcw5038@gmail.com>
2018-09-02 20:49:48 -0700

    Added initial testing and setup

--- server.js
+++ server.js
@@ -3,4 +3,49 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.use(express.json());
+//app.listen(process.env.PORT || 8080);
+
+app.use(express.static('public'));
+
+let server;
+
+// this function starts our server and returns a Promise.
+// In our test code, we need a way of asynchronously starting
+// our server, since we'll be dealing with promises there.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
