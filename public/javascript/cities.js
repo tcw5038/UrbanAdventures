1cb84d136166140e4601466ab502a867a009a4ef
Tyler Warrick <tcw5038@gmail.com>
2018-09-11 12:38:20 -0700

    Finished basic front end structure with some visual design

--- public/javascript/cities.js
+++ public/javascript/cities.js
@@ -1,10 +1,24 @@
 'use strict';
 /* global $ */
 
-$('.x').click(function(e){
-  $('.dark-box').hide();
-});
+$(".close").click(function(e){
+  $(".citiescontainer").hide();
+})
 
-$('.open').click(function(e){
-  $('.dark-box').show();
-});
+$(".open").click(function(e){
+  $(".citiescontainer").show();
+})
+function initMap(data) {
+  let lat = 0;
+  let lng = 0;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: lat, lng: lng },
+    zoom: 4,
+    draggable: true,
+    zoomControl: true,
+    scrollWheel: false,
+    gestureHandling: 'greedy'
+  });
+}
+
+initMap();
