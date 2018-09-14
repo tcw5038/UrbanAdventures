e1799bc8d42bed398770ea4acb3eef41a879e760
Sebastian Bastidas <sbastidasr@gmail.com>
2018-09-14 16:46:33 -0500

    Floats

--- public/javascript/cities.js
+++ public/javascript/cities.js
@@ -1,36 +1,35 @@
-'use strict';
+"use strict";
 /* global $ */
 
 /*FOR HIDING AND SHOWING THE CITIES BESIDE THE MAP */
-$(".close").click(function(e){
+$(".close").click(function(e) {
   $(".citiescontainer").hide();
-})
+});
 
-$(".open").click(function(e){
+$(".open").click(function(e) {
   $(".citiescontainer").show();
-})
+});
 
 /*FOR INITIALIZING GOOGLE MAPS */
 let map;
 function initMap(data) {
   let lat = 0;
   let lng = 0;
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: lat, lng: lng },
     zoom: 4,
     draggable: true,
     zoomControl: true,
     scrollWheel: false,
-    gestureHandling: 'greedy'
+    gestureHandling: "greedy"
   });
 }
 
-
 /* FOR OPENING AND CLOSING CITY DETAIL PAGES */
-$(".x").click(function(e){
+$(".x").click(function(e) {
   $(".darken").hide();
-})
+});
 
-$(".opencity").click(function(e){
+$(".opencity").click(function(e) {
   $(".darken").show();
-})
+});
