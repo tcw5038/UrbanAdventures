83cf9b609603c66c158196bf483f5077f5e30d3b
Tyler Warrick <tcw5038@gmail.com>
2018-09-17 18:19:47 -0700

    Improved navigation, added form for new city, added button for adding cities

--- public/javascript/cities.js
+++ public/javascript/cities.js
@@ -27,9 +27,59 @@ function initMap(data) {
 
 /* FOR OPENING AND CLOSING CITY DETAIL PAGES */
 $(".x").click(function(e) {
-  $(".darken").hide();
+  $(".darken-detail").hide();
 });
 
 $(".opencity").click(function(e) {
-  $(".darken").show();
+  $(".darken-detail").show();
 });
+
+
+/* FOR ADDING A NEW CITY AND CLOSING OUT OF THE ADD PAGE */
+$(".create-city-button").click(function(e){
+  $(".darken-add-city").show();
+});
+
+$(".x").click(function(e) {//closes out without adding the new city since the user chose to x out of the page
+  $(".darken-add-city").hide();
+});
+
+$(".submit-city").click(function(e) {//closes out when the user hits submit...not sure if this is necessary or how to implement??
+  $(".darken-add-city").hide();
+});
+
+/* FUNCTIONS FOR CREATING AND RENDERING THE CITIES TO THE PAGE */
+
+function getCities(){//gets all the cities associated with a given user ID
+
+}
+
+function generateCityHTML(){//generates the HTML for each individual city
+
+}
+
+function renderCities(){//renders all of the cities to the page using the generateCityHTML function
+
+}
+
+function handleCityClicked(){//brings up the city detail page
+
+}
+
+
+
+function handleFormSubmit(){//empties the form and calls createCity when the user decides to add a new city
+
+}
+
+function createCity(){//creates a new city using the form data inputted
+
+}
+
+/*FUNCTIONS RELATED TO ERROR HANDLING */
+
+function generateError(){//generates an error if necessary
+
+}
+
+/*FUNCTIONS RELATING TO RENDERING PINS OF THE CITIES ON THE MAP */
