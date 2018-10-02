8412a325c470a9a6bc4fc3f0c92eebb573dc184a
Tyler Warrick <tyler@tylers-mbp.attlocal.net>
2018-10-01 20:21:39 -0700

    Committing latest changes

--- public/javascript/cities.js
+++ public/javascript/cities.js
@@ -27,9 +27,16 @@ $(".x").click(function(e) {
   $(".darken-detail").hide();
 });
 
+/*
+$(".submit-city").submit(function(e) {
+  $(".darken-detail").toggle();
+});*/
+
+
+/*
 $(".opencity").click(function(e) {
   $(".darken-detail").show();
-});
+});*/
 
 
 /* FOR ADDING A NEW CITY AND CLOSING OUT OF THE ADD PAGE */
@@ -48,21 +55,22 @@ $(".x").click(function(e) {//closes out without adding the new city since the us
 /* FUNCTIONS FOR CREATING AND RENDERING THE CITIES TO THE PAGE */
 
 function getCity(userID, callback){//gets one city that the user clicks on
-  let parameters = {
-
-  }
+  
 }
 
 function getCities(user, callback){//gets all the cities when a signed in user goes to their dashboard
-  url:
-  type:,
-  datatype:,
-  success:,
-  error:,
+  $.ajax({
+    type: 'GET',
+    url: `/api/cities/`,//this needs an associated id of some kind to make sure we know whos cities we are planning to get
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    datatype: 'json',
+  });
 }
 
 function generateCityHTML(city){//generates the HTML for each individual city
-   return `<div class="city-card"><p class="cityname">${city.name}</p></div>`;
+   return `<div class="city-card"><p class="cityname">${city.cityName}</p></div>`;
 }
 
 function renderCities(cities){//renders all of the cities to the page using the generateCityHTML function
@@ -73,16 +81,41 @@ function renderCities(cities){//renders all of the cities to the page using the 
 
 function handleCityClicked(){//brings up the city detail page, recall that the lab will be on the image so image needs to be darkened
   $('.city-card').on('click', function(e){//when a city card div is clicked, open that city card
-    $(".darken-detail").show();
-
+    console.log('city clicked');
+    $('.darken-detail').show();
   });
 }
 
-function handleUpdateCity(){//handles user requests to update a given city
+function handleUpdateCityClicked(){//handles user requests to update a given city
   $('.edit-city').on('click', function(){//when the edit city button is clicked, allow the edits to be made
     //make all fields editable
     //make a put request with the updated information from all of the fields
+    updateCity(updatedCityObject);
+  });
+}
 
+function updateCity(){
+//ajax put request
+
+
+}
+
+function handleDeleteCityClicked(id){//handles user requests to delete a given city
+  $('.delete-city').on('click', function(){//when the delete city button is clicked, the city will be deleted
+    deleteCity();//need to figure out how to target the specific city that we want to delete in order to pass it in...target it then store it in a variable
+    //once this is working well, consider adding an "are you sure?" kind of popup box to confirm
+  });
+}
+
+function deleteCity(){
+  $.ajax({
+    type: 'DELETE',
+    url: `/api/cities/${id}`,
+    data: JSON.stringify(deleteCity),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    datatype: 'json',
   });
 }
 
@@ -100,20 +133,20 @@ function getCheckboxValues(){//gets the values of whatever is checked in the che
     return checkedVals;
 }
 
-function createCity(){//creates a new city using the form data inputted
+
+function createCityObject(){
   $('form').on('submit', function(){//be sure to have a condition that checks to see if all of the required fields are filled out
-    console.log('Just checking to see if anything is happening!');
-    event.preventDefault();
+    console.log('Just checking to see if anything is happening!!!!');
     //set all of the variables to whatever is inside of the form submission
 
     let cityName = $("#cityName").val();
     let country = $("#country").val();
-    let yearVisited = $("#date").val();
+    let yearVisited = $("#yearVisited").val();
     let notes = $("#notes").val();
     let tags = getCheckboxValues();
     let imageURL = $("#imageURL").val();
 
-    console.log(`${cityName}, ${country}, ${yearVisited}, ${notes}, ${tags}, ${imageURL}`)//just for testing purposes
+    console.log(`${cityName}, ${country}, ${yearVisited}, ${notes}, ${tags}, ${imageURL}`);
 
     let newCity = {//creates a new city object using the information stored above
       cityName:cityName,
@@ -123,37 +156,46 @@ function createCity(){//creates a new city using the form data inputted
       tags:tags,
       image:imageURL
     }
+    //$('.darken-detail').hide();//why isnt this working?????
     console.log(newCity);
-
-    const addNewCity = {
-      type: "POST",//make sure to fix these based on whatever I decide
-      url: `/api/cities/`,//this is not working currently, something to do with localhost maybe?
-      data: JSON.stringify(newCity),
-      success: function(response){
-        $('.citiescontainer').append(generateCityHTML(response));//generates the new city based on the response data
-      },
-      error: function(response) {
-        console.error(response);
-      }
-    };
-   $.ajax(addNewCity);
+    storeCity(newCity, generateCityHTML);
   });
+}
+
+function storeCity(city, callback){//creates a new city using the form data inputted
+     console.log(city);
+
+    $.ajax({
+      type: 'POST',
+      url: `/api/cities`,
+      data: JSON.stringify(city),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .done(function(response){
+      callback(response);
+    })
+    .fail(function(err){
+      generateError(err);
+    })
 }
 
 
 
 /*FUNCTIONS RELATED TO ERROR HANDLING */
 
-function generateError(){//generates an error if necessary
-
+function generateError(error){//generates an error if necessary
+  console.error(error)
 }
 
 /*FUNCTIONS RELATING TO RENDERING PINS OF THE CITIES ON THE MAP */
 
 
 $(function () {
-handleCityClicked();
 handleFormSubmit();
-createCity();
-handleUpdateCity();
+createCityObject();
+handleUpdateCityClicked();
+handleDeleteCityClicked();
+handleCityClicked();
 });
