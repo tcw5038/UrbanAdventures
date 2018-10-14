c8cce1c45300b3bd3b81b28cfdc14704f7afbb79
Tyler Warrick <tcw5038@gmail.com>
2018-10-13 23:59:05 -0700

    Committing latest changes to continue on laptop

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,12 +7,20 @@ function handleLoginClicked(){//handles when the user clicks to login
         let email = $("#email").val();
         let password = $("#password").val();
      });
+     console.log(loginInfo);
     logInUser(loginInfo);
 }
 
 function logInUser(loginInfo){//makes ajax call to get the users information
     $.ajax({
-
+        url:'/api/auth/login',//go back and check to make sure this is right
+        method: 'POST',
+        data:JSON.stringify(loginInfo),
+        headers: {
+            'content-type':'application/json'
+        }
+        
+        
 
 
     })
