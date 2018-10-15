e84e5ccc3bd1e79411c4baed4da0265a713f62b6
Sebastian Bastidas <sbastidasr@gmail.com>
2018-10-15 22:41:39 +0200

    Changes

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -17,40 +17,45 @@ function createUserObject(){
 
   console.log(firstName);
 
-  let newUser = {//creates the object using the values obtained in the fields above
+  let user = {//creates the object using the values obtained in the fields above
     firstName:firstName,
     lastName:lastName,
     email:email,
+    username:email,
     password:password
   }
-  console.log(newUser);//checking to make sure that everything worked so far
+  console.log(user);//checking to make sure that everything worked so far
 
-  createUser(newUser);//post request to create the new user
+  createUser(user);//post request to create the new user
 }
 
 function createUser(user){
 
   let settings = {
-    url: `/api/users/`,
     method: 'POST',
+    data:JSON.stringify(user),
     headers: {//if i put this in i get 500 if i take this out i get 422
       'content-type': 'application/json',
     },
   }
   //ajax put request
-  $.ajax(Object.assign({}, settings, {data:JSON.stringify(user)}))
-  .then(() => {
+  $.ajax(Object.assign({}, settings, {url: `/api/users/`}))
+  .done(() => {
     console.log(`User with the email address: ${user.email} added to database`);
-    $.ajax(Object.assign({}, settings, {data:JSON.stringify({email, password})}))
-  })
-  .then((res) => {
-    console.log("User successfully logged in", res);
-    localStorage.setItem('authToken', res.authToken);
-    localStorage.setItem('username', username);
-    window.location.href = "cities.html";
+    $.ajax(Object.assign({}, settings, {url: `/api/auth/login`}))
+    .done((res) => {
+      console.log("User successfully logged in", res);
+      localStorage.setItem('authToken', res.authToken);
+      localStorage.setItem('username', email);
+      window.location.href = "cities.html";
+    })
+    .fail(function(err){
+      console.error(err);
+      //put login errors here and generate the html either here or somewhere else
+    })
   })
 
-  .catch(function(err){
+  .fail(function(err){
     console.error(err);
     //put login errors here and generate the html either here or somewhere else
   })
