d8006af75de556ba34968e7b86c381e613302ac8
Tyler Warrick <tyler@tylers-mbp.attlocal.net>
2018-10-20 11:44:00 -0700

    Trying to fix heroku issue

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const passport = require("passport");
 
-// Here we use destructuring assignment with renaming so the two variables
-// called router (from ./users and ./auth) have different names
-// For example:
-// const actorSurnames = { james: "Stewart", robert: "De Niro" };
-// const { james: jimmy, robert: bobby } = actorSurnames;
-// console.log(jimmy); // Stewart - the variable name is jimmy, not james
-// console.log(bobby); // De Niro - the variable name is bobby, not robert
 const { router: usersRouter } = require("./users");
 const { router: citiesRouter } = require("./cities");
 const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
@@ -21,6 +14,7 @@ mongoose.Promise = global.Promise;
 const { PORT, DATABASE_URL } = require("./config");
 
 const app = express();
+app.use(express.static("public"));
 
 // Logging
 app.use(morgan("common"));
@@ -36,7 +30,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static("public"));
+
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
