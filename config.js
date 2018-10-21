68fbc06e335032f743f20cab37a0d7d2ca18eef4
Tyler Warrick <tyler@tylers-mbp.attlocal.net>
2018-10-20 22:57:30 -0700

    Reverting to previous version

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tylerw:Jydm8YddyYGfexf@ds137283.mlab.com:37283/urban-adventures';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/urbanadventures-test';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://tylerw:Jydm8YddyYGfexf@ds137263.mlab.com:37263/urban-adventures-test';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'mysecret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
