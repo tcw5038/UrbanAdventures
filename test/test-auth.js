de1473c87208492a77906feb32faab2c7b1ab7c9
Tyler Warrick <tyler@tylers-mbp.attlocal.net>
2018-10-20 22:38:16 -0700

    Attempt to fix tests

--- test/test-auth.js
+++ test/test-auth.js
@@ -10,9 +10,7 @@ const { JWT_SECRET, TEST_DATABASE_URL } = require('../config');
 
 const expect = chai.expect;
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
+
 chai.use(chaiHttp);
 
 describe('Auth endpoints', function () {
@@ -65,7 +63,7 @@ describe('Auth endpoints', function () {
       return chai
         .request(app)
         .post('/api/auth/login')
-        .send({ email: 'wrongemail', password })
+        .send({ email: 'wrongemailjiojij', password })
         .then(() =>
           expect.fail(null, null, 'Request should not succeed')
         )
